f7676e7ce92d099a23d1c79c82657e86d06ed339
Abhai2016 <abhai2013@gmail.com>
2025-06-22 15:39:19 +0200

    Got familiar with quick sort

--- src/SortAlgorithms.java
+++ src/SortAlgorithms.java
@@ -115,30 +115,29 @@ public class SortAlgorithms {
             return;
         }
 
-        int pivotIndex = partition(arr, start, end);
+        int pivotIndex = divideAndConquer(arr, start, end);
         quickSort(arr, start, pivotIndex);
         quickSort(arr, pivotIndex + 1, end);
     }
 
-    private static int partition(int[] arr, int start, int end) {
+    private static int divideAndConquer(int[] arr, int start, int end) {
         int pivot = arr[start];
         int i = start;
         int j = end;
 
         while (i < j) {
-            while (i < j && arr[--j] >= pivot);
+            while (i < j && arr[--j] >= pivot) {}
             if (i < j) {
                 arr[i] = arr[j];
             }
 
-            while (i < j && arr[++i] <= pivot);
+            while (i < j && arr[++i] <= pivot) {}
             if (i < j) {
                 arr[j] = arr[i];
             }
-
         }
 
-        arr[j] = pivot;
-        return j;
+        arr[i] = pivot;
+        return i;
     }
 }
